88a324bbae20bc92cfa319d761649ccc94839ebd
imranyy <imranmatano6@gmail.com>
2023-01-27 18:19:11 +0300

    wait for psyche to build up then continue

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,5 +1,15 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import search from "../../db/search.json";
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req, res) {
+  if(req.method==='POST'){
+    try {
+      res.status(200).json({
+        mess:'it okay',
+        output:search
+      })
+    } catch (error) {
+      res.status(400).json({msg:"Bad Request, Try again!"})
+    }
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -29,18 +29,95 @@ export default function Home() {
         text.textContent+=goal.charAt(curr);
         curr++;
         if(curr<goal.length){
-            setTimeout(write,50)
+            setTimeout(write,100)
+        }
+    }
+    const goal1='ensuring healthy living and protect sick people from developing diseases..'
+    var curr1=0;
+    const write1=()=>{
+        const text=document.querySelector('.small-first');
+        text.textContent+=goal1.charAt(curr1);
+        curr1++;
+        if(curr1<goal1.length){
+            setTimeout(write1,100)
         }
     }
     useEffect(()=>{
       write();
+      setTimeout(()=>{
+        write1()
+      },5000)
     },[])
     
     const start=()=>{
       setStartInfo([]);
       document.querySelector('.small-first-text').textContent=''
       document.querySelector('.new-check-up').classList.add('checked')
+      document.querySelector('form').style.opacity=1;
+    }
 
+    const [input,setInput]=useState('');
+    const [data,setData]=useState([]);
+    const [aiResponse,setAiResponse]=useState([]);
+    const [btnIcon,setBtnIcon]=useState(<i className="fa fa-send"></i>);
+    const handleSubmit=async(e)=>{
+      e.preventDefault()
+      const form=document.querySelector('form');
+      try {
+        setBtnIcon(<i className="fa fa-rocket"></i>)
+        const url='/api/'
+        const response=await fetch(url,{
+          method:"POST",
+          headers:{
+            'content-type':'application/json'
+          },
+          body:JSON.stringify({
+              prompt:input
+          })
+        })
+        setBtnIcon(<i className="fa fa-send"></i>)
+        form.reset()
+        const parseRes=await response.json();
+        if(parseRes.error){
+          setData(
+            <div className={styles.textError}>
+              <p style={{color:'red'}}>Error: {parseRes.msg}</p>
+            </div>
+          )
+          setTimeout(()=>{
+            setData('')
+          },8000)
+        }else{
+          setAiResponse(parseRes.mess)
+          setData(parseRes.output);
+          
+        var curr=0;
+        const hey=data.organic[0].title
+        const hy=document.querySelector('.hey')
+            hy.textContent+=hey.charAt(curr);
+            curr++;
+            if(curr<hey.length){
+                setTimeout(write,50)
+            }
+        
+        }
+      } catch (error) {
+        console.log(error.message)
+        form.reset()
+        setBtnIcon(<i className="fa fa-send"></i>)
+        setData(
+          <div className={styles.textError}>
+            <p style={{color:'red'}}>Error: No Internet</p>
+          </div>
+        )
+        setTimeout(()=>{
+          setData('')
+        },8000)
+      }
+    }
+
+    const clearScreen=()=>{
+      setTextResponse('')
     }
   return (
     <div>
@@ -62,12 +139,13 @@ export default function Home() {
             </ul>
             <div className={styles.info}>
                 <ul>
-                    <li className='clear-screen'><i className="fa fa-trash add"></i> Delete Chats</li>
+                    <li onClick={clearScreen}><i className="fa fa-trash add"></i> Delete Chats</li>
                 </ul>
             </div>
         </div>
         <div className={styles.window}>
             <small className='small-first-text'></small><br/>
+            <small className='small-first'></small><br/>
             <div className={styles.startInfo}>
               {startInfo.map(i=>(
                 <div className={styles.tab} key={i.id}>
@@ -76,11 +154,39 @@ export default function Home() {
               ))}
             </div>
 
-            <div className="text-response"></div>
+            <div className={styles.textResponse}>
+            <p className={styles.req}> {input}  <small> <i>AI's response: {aiResponse}</i></small></p>
+            <div className={styles.text} title={`${input} 's response`}>
+                  <div key={data.organic&&data.organic[0].position}>
+                        <p className='hey'></p>
+                        {/* <p>{data.organic&&data.organic[0].snippet}</p>
+                        <a href={data.organic&&data.organic[0].link} target='_blank' rel='noreferrer'>View more...</a><br/>
+                        <br/> */}
+            </div>
+                    {/* <div key={data.organic&&data.organic[1].position}>
+                        <p>{data.organic&&data.organic[1].title}</p>
+                        <p>{data.organic&&data.organic[1].snippet}</p>
+                        <a href={data.organic&&data.organic[1].link} target='_blank' rel='noreferrer'>View more...</a><br/>
+                        <br/>
+                    </div>
+                    <div key={data.organic&&data.organic[2].position}>
+                        <p>{data.organic&&data.organic[2].title}</p>
+                        <p>{data.organic&&data.organic[2].snippet}</p>
+                        <a href={data.organic&&data.organic[2].link} target='_blank' rel='noreferrer'>View more...</a><br/>
+                        <br/>
+                    </div>
+                    <div key={data.organic&&data.organic[3].position}>
+                        <p>{data.organic&&data.organic[3].title}</p>
+                        <p>{data.organic&&data.organic[3].snippet}</p>
+                        <a href={data.organic&&data.organic[3].link} target='_blank' rel='noreferrer'>View more...</a><br/>
+                        <br/>
+                    </div> */}
+                </div>
+            </div>
             <div className="down"></div>
-            <form className="handle-submit">
-              <input className="playground-textarea" name='request' placeholder="Signs or symptoms" required/>
-              <button className="playground-submit-btn"></button>
+            <form className="handle-submit" onSubmit={handleSubmit}>
+              <input className="playground-textarea" onChange={e=>setInput(e.target.value)}  placeholder="Signs or symptoms" required/>
+              <button className="playground-submit-btn">{btnIcon}</button>
             </form>
         </div>
       </div>
